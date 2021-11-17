164aea321c390fa23fb84e75412e93c143a30cb6
zer0 <zer0@example.com>
2021-11-17 09:27:12 +0900

    connected react to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
+import axios from 'axios'
 
 function App() {
+
+  axios.get('http://localhost:3001/')
+    .then(res => console.log(res))
+    .catch((err) => console.log(err))
+
   return (
     <div className="App">
       <h1>hello world</h1>
--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors({
+    origin: '*'
+}));
+
 
 app.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
     res.send("hello from server")
 })
 
