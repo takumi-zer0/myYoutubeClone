01baab6788969d4231b781b8eb8864d23b71b444
Takumi Ueno <zer0@Takumis-MacBook-Pro.local>
2021-11-27 19:20:43 +0900

    day one

--- server/server.js
+++ server/server.js
@@ -13,4 +13,9 @@ app.get('/', (req, res) => {
     res.send("hello from server")
 })
 
+app.get('/video', (req, res) => {
+    var info = {video_title: 'titlefldsjhk', video_thumb: './images/thumb.png', video_length: 'length:int',video_views: '1000', video_time: 'release date', user_prof:'profpath', user_name: 'channel name', }
+    res.send(info)
+})
+
 app.listen(3001)
